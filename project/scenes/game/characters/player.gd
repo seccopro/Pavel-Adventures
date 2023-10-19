cad31814322f3c59e5dc9e19e9c94e369a67a52a
duplo8 <MGR.rupel@gmail.com>
2023-10-19 18:28:57 +0200

    player script clean-up and functions improvement
    made script more scalable and performant  by making less code run every frame

--- project/scenes/game/characters/player.gd
+++ project/scenes/game/characters/player.gd
@@ -2,7 +2,6 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-var jump_charge=0
 const CLIMBING_SPEED = 100.0
 const DASH_SPEED = 1000
 const DASH_DURATION = 30
@@ -13,11 +12,9 @@ var is_climbing = false
 var is_dashing = false
 var is_alive = true
 
-
-
-
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -30,60 +27,64 @@ func _physics_process(delta):
 	
 	move_and_slide()
 	movement()
-	
+
 
 func movement():
 	#basic left and right
 	var direction = Input.get_axis("left", "right")
-	if direction and not is_climbing and not is_dashing:
+	if direction and not is_climbing and not is_dashing:	#locked during climb and dash
 		velocity.x = direction * SPEED
 	else:	#slowing down after releasing
 		velocity.x = move_toward(velocity.x, 0, 50)
+	
 # Handle Jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 	
-	
-	if Input.is_action_just_pressed("dash") and not is_climbing:		# GETS RESET BY NORMAL SPEED OF WALKING (use flag)
-		if(Input.is_action_pressed("right")): #moving right
+	if Input.is_action_just_pressed("dash") and not is_dashing and not is_climbing: #maybe to add dash cooldown 
+		dash()
+	elif is_dashing:
+		dash_reset()	#to reset movement after dash
+			
+	#start climbing / attach to the wall
+	if Input.is_action_pressed("climb") and is_on_wall():
+		is_climbing = true
+	if is_climbing:
+		climb()
+		
+func climb():	
+#climb up and down			
+	var climb_direction = Input.get_axis("up", "down")		
+	if climb_direction:
+		velocity.y = climb_direction * CLIMBING_SPEED
+	else:	#slowing down after releasing
+		velocity.y = move_toward(velocity.y, 0, CLIMBING_SPEED) 	#makes player detach on the end of a wall
+		
+# Handle climb-jump
+	if Input.is_action_just_pressed("jump"):	#should jump to the direction opposite of the wall
+		velocity.y = JUMP_VELOCITY
+		is_climbing = false
+		
+#stop climbing, any moment that you aren't touching the wall	
+	if not is_on_wall():
+		is_climbing=false
+
+func dash():
+	#dash while moving right (checking input, not momentum, nor character facing direction)
+		if(Input.is_action_pressed("right")): 
 			is_dashing = true;
 			velocity.x += DASH_SPEED
 			remaining_dashing = DASH_DURATION
-				
-		elif (Input.is_action_pressed("left")): #moving left
+	#dash while moving left (checking input, not momentum, nor character facing direction)
+		elif (Input.is_action_pressed("left")): 
 			is_dashing = true;
 			velocity.x -= DASH_SPEED
 			remaining_dashing = DASH_DURATION
-#waiting for dash to finish
-	if is_dashing and remaining_dashing>0:
-		remaining_dashing-=1 #should wait DASH_DURATION cicles to let you move again (reset your speed)
-	elif is_dashing:
-		is_dashing=false
-		print("dash reset")	
-	
-	
-	if is_on_wall():
-		climbing()
-		
-func climbing():
-	#start climbing
-	if Input.is_action_pressed("climb"):
-		is_climbing = true
-		print("climbing")
-	
-	
-	#climb up and down			
-	var climb_direction = Input.get_axis("up", "down")	
-	
-	if is_climbing and climb_direction:
-		velocity.y = climb_direction * CLIMBING_SPEED
-	elif is_climbing:	#slowing down after releasing
-		velocity.y = move_toward(velocity.y, 0, CLIMBING_SPEED)
-	
-	# Handle climb-jump
-	if is_climbing and Input.is_action_just_pressed("jump"):
-		velocity.y = JUMP_VELOCITY
-		is_climbing = false
-		print("not climbing")
-
-
+			
+func dash_reset():
+	#waiting for dash to finish			SHOULD CHECK HORIZONTAL MOMENTUM TO RESET
+		if remaining_dashing>0:
+			remaining_dashing-=1 #waits for  ${DASH_DURATION} cicles to let you move again (reset your speed)
+		else:
+			is_dashing=false
+			print("dash reset")	
