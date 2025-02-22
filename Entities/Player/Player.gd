7e1402faedc81cfe851a1ce2f573dcc3475fb63c
duplo8 <MGR.rupel@gmail.com>
2025-02-22 17:22:21 +0100

    player gravity, deactivated vsync for flicker/stutter/sfarfallamento

--- Entities/Player/Player.gd
+++ Entities/Player/Player.gd
@@ -8,8 +8,10 @@ extends CharacterBody2D
 @onready var animation_player: AnimationPlayer = %AnimationPlayer
 @onready var animation_tree: AnimationTree = %AnimationTree
 
-var move_direction: Vector2
-const SPEED: float = 100.0
+const SPEED: float = 300.0
+const JUMP_VELOCITY: float = -400.0
+
+var gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("move_left") or event.is_action_pressed("move_right"):
@@ -22,10 +24,22 @@ func _process(_delta: float) -> void:
 	pass
 
 func _physics_process(delta: float) -> void:
-	if not input_enabled:
-		return
+	if not is_on_floor():
+		velocity.y += gravity * delta
 	
-	#move_and_slide()
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	
+	var direction: float = Input.get_axis("move_left", "move_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, delta)
+	
+	#if not input_enabled:
+	#    return
+	
+	move_and_slide()
 
 func disable() -> void:
 	input_enabled = false
